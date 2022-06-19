06f5e14a8adee103aae572510ab4aff6d31d97ec
CrooKameron <85321458+CrooKameron@users.noreply.github.com>
2022-06-19 07:30:43 +0000

    a

--- 1 - js basics/7 arrays.js
+++ 1 - js basics/7 arrays.js
@@ -1,6 +1,31 @@
-var allah = false;
-if (allah) {
-  console.log("allah var");
-} else {
-  console.log("allah yok");
+ let products = ["iphone 12","iphone 13","iphone 13 pro"]; //assigning array variables as string
+let prices = [9000, 12000 ,14000]; // same shit but its numbers
+let colors = ["black","space blue","gray"]; // same shit but its numbers
+
+var productselection = 1; // in here we can choose the productlsls
+
+////////////////////////////////////////////// printing the selected product name, price and color
+console.log                      ///////////
+(                                /////////
+  " product name: " +            ///////
+  products[productselection]     ///// 
+);                               ////
+console.log                      //
+(                                //
+  " price: " +                   //
+  prices[productselection]       //
+)                                //
+console.log                      //
+(                                //
+  " Color: " +                   //
+   colors[productselection]      //
+)                                //
+                                 //
+///////////////////////////////////
+
+// or we can just print the whole array list
+for (let a = 0; a < products.length; a++) {
+  console.log(products[a]);
 }
+////////////////////////////////////////////
+
