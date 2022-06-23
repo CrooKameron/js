183c637c40f600f711e70c19faf8c2f238bc2d10
CrooKameron <85321458+CrooKameron@users.noreply.github.com>
2022-06-23 09:50:44 +0000

    a

--- 1 - js basics/practise/array/(FP) student database.js
+++ 1 - js basics/practise/array/(FP) student database.js
@@ -1,8 +1,8 @@
-let//          birth date v |  v  v  v exam scores
-  stdnt1 = ["deniz bilgi",2010,70,60,80,71],
-  stdnt2 = ["ada bilgi"  ,2012,80,80,90,72],
-  stdnt3 = ["ahmet kaya" ,2009,60,60,70,73];
-//                                      ˆ school numbers
+//             birth date vvvv |  vvvvvvvvvv exam scores
+  stdnt1 = ["deniz bilgi",2010,70,[60,80,71]],
+  stdnt2 = ["ada bilgi"  ,2012,80,[80,90,72]],
+  stdnt3 = ["ahmet kaya" ,2009,60,[60,70,73]];
+//                             ˆ school numbers
 
 let currenttime = new Date();               // creating a new date variable    //
 let currentyear = currenttime.getFullYear();//                                 // making a new variable that has current year in it 
@@ -13,8 +13,7 @@ let studentselection = stdnt2; // selecting the student
 let selectedstudentid = studentselection[5];
 let studentname  = studentselection[0];
 let studentage   = currentyear - studentselection[1];
-let studentscore = (studentselection[2] + studentselection[3] + studentselection[4])/3;
-
+let studentscore = ((studentselection[3][0]) + (studentselection[3][1]) + (studentselection[3][2])) / 3;
 
 console.log("vvvv  "+"selected student id: "+selectedstudentid+"  vvvvv");
 console.log("name and surname: "+studentname);
