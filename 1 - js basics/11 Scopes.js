ad17cbb9a013d084c583c808ee6d9678502e3b35
CrooKameron <85321458+CrooKameron@users.noreply.github.com>
2022-07-30 19:11:15 +0000

    a

--- 1 - js basics/11 Scopes.js
+++ 1 - js basics/11 Scopes.js
@@ -1,8 +1,22 @@
 var name = "ahmet";
 
-function func1() { // functions first looks for the variables if it cant find used variables inside itself it uses the global variables 
+const adress = "istanbul"; // const variables are unchangeable 
+adress = "izmir"; // if you try to chnage a const variable you'll get error 
+
+function func1() { //  if functions cant find used variables inside itself it uses the global variables 
   var name = "ayşe";
   console.log(name);
 }
+
+if (true) {
+  let gender = "attack chopper"; // let variables are only can useable inside blocks
+  var name = "zeynep";
+  console.log(name, gender)
+}
+console.log(gender); // like you can see if you try to use let variables that defined anc created in another block you will get error
+
+
+//you cant create a new scope in a block (let,const)
+//functions creates their own scope zones
 
 func1();
